d7d44f1d04218e6595605d0ca794d22c4e28d16b
Yeoh zhou <yeohzhou@YeohzhoudeMacBook-Pro.local>
2016-11-16 15:43:27 +0800

    rsync2
    内网用 直接输入ip就可以 自动生成c段ip

---
+++ rsyncCheck2.py
@@ -0,0 +1,39 @@
+import commands
+from multiprocessing.dummy import Pool as ThreadPool
+from sys import argv
+import os
+
+def rsyncExec(ip):
+	try:
+		out = [ip]
+		cmd = 'rsync '+ip+'::'
+		(status,output) = commands.getstatusoutput('rsync '+ip+'::')
+		if status == 0:
+			model = output.split()
+		else:
+			model = '1'
+		if model != '1':
+			for x in model:
+				(statusCheck,outputCheck) = commands.getstatusoutput('rsync '+ip+'::'+x+'/')
+				if statusCheck == 0:
+					out.append(x)
+		if len(out) > 1:
+			return out
+	except Exception as e:
+		raise e
+
+if __name__ == '__main__':
+	ip,threed_num = argv[1:]
+	ips = [str(ip)+ '.' +str(x) for x in range(255)]
+	pool = ThreadPool(int(threed_num))
+	results = pool.map(rsyncExec,ips)
+	pool.close()
+	pool.join()
+	fp = open('rsyncOut.txt','w')
+	ret =  [str for str in results if str not in ['', ' ', None]]
+	for x in ret:
+		fp.write(x[0]+'    ')
+		for i in x[1:]:
+			fp.write(" '"+i+"'")
+		fp.write('\n')
+	fp.close
